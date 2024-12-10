622ae5ba0c2ad7c878674cc0e3e8488a5e0930f8
GitHub Action <action@github.com>
2024-12-10 10:57:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/kevinmwangi/intellicode/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kevinmwangi/intellicode/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.kevinmwangi.intellicode.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kevinmwangi.intellicode.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/kevinmwangi/intellicode/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/kevinmwangi/intellicode/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.kevinmwangi.intellicode.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.kevinmwangi.intellicode.MyBundle
+import com.github.kevinmwangi.intellicode.services.MyProjectService
 import javax.swing.JButton
 
 
